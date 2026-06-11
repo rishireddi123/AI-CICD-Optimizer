7a11b161fcf16c564864bcbe8fbe0a3873350366
RishiRedd123 <rishispam25@gmail.com>
2026-06-11 15:04:12 -0400

    Day 1: Add Python app, tests, and CI pipeline

---
+++ tests/test_calculator.py
@@ -0,0 +1,15 @@
+from app.calculator import add, subtract, multiply
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+
+def test_divide():
+    assert add(10, 2) == 15 
+
+    
